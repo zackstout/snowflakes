7280a446ed2badf4e528eac133c6fca595ca7789
Zack Stout <zackstout@gmail.com>
2018-08-09 22:05:14 -0500

    fixing dims

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,11 +1,14 @@
 
 var angle2 = 10.1;
 var colorPairs = [[], ['steelblue', 'orange'], ['tomato', 'grey'], ['green', 'purple']];
+var w, h;
 
 function setup() {
-  createCanvas(500, 500);
+  w = window.innerWidth;
+  h = window.innerHeight;
+  createCanvas(w, h);
   background(200);
-  drawHex(250, 250, 120, 90, 3);
+  drawHex(w/2, h/2, h/4, h/6, 3);
 
   // drawMoreHex(250, 250, 70);
 }
@@ -13,7 +16,7 @@ function setup() {
 function draw() {
   background(200);
   angle2 += 0.03;
-  drawHex(250, 250, 100, 70, 3);
+  drawHex(w/2, h/2, h/4, h/6, 3);
 }
 
 function drawMoreHex(cx, cy, s) {
